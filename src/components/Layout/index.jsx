ec2c5abbdf6ef37d78031dec0e39637c9f99d944
Natalia <natalia.araujo@yazo.com.br>
2022-09-03 00:22:58 -0300

    improviment: styled component Layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,14 +1,15 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import { Profile } from "./Profile"
+import { Profile } from "../Profile"
+import { LayoutMain, LayoutWrapper } from "./style"
 
 export const Layout = ({ children }) => (
-  <>
+  <LayoutWrapper>
     <aside>
       <Profile />
     </aside>
-    <main>{children}</main>
-  </>
+    <LayoutMain>{children}</LayoutMain>
+  </LayoutWrapper>
 )
 
 Layout.propTypes = {
